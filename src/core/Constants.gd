517dcbdebbb8810697cdd48c96bd35ade5b8d228
Doscharl <dosso.choilio@gmail.com>
2026-06-24 11:15:37 +0000

    Create Constants.gd for board setup and valid cells
    Added initial board setup and valid cell retrieval function.

---
+++ src/core/Constants.gd
@@ -0,0 +1,12 @@
+# src/core/Constants.gd
+extends Node
+
+const INIT_BOARD: Dictionary = {
+    # Placeholder for initial board setup
+}
+
+func get_valid_cells() -> Array:
+    return ["D1", "C2", "D2", "E2", "A3", "B3", "C3", "D3", "E3", "F3", "G3", 
+            "A4", "B4", "C4", "D4", "E4", "F4", "G4", "A5", "B5", "C5", "D5", 
+            "E5", "F5", "G5", "A6", "B6", "C6", "D6", "E6", "F6", "G6", 
+            "A7", "B7", "C7", "D7", "E7", "F7", "G7", "C8", "D8", "E8", "D9"]
